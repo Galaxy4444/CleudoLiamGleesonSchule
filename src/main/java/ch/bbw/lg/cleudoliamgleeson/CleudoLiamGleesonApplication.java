968eb03cf1c35400e556afee3a1d209d054bdb31
glees <zgleel@bbcag.ch>
2022-09-01 09:05:03 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/lg/cleudoliamgleeson/CleudoLiamGleesonApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.lg.cleudoliamgleeson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleudoLiamGleesonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleudoLiamGleesonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/lg/cleudoliamgleeson/CleudoLiamGleesonApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.lg.cleudoliamgleeson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleudoLiamGleesonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
